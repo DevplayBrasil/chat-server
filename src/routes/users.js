b0e84e656fd92d465d385f4622c09835ab443287
Arthur Mendes <thumendess@gmail.com>
2022-10-10 17:22:08 -0300

    aula07

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,14 +3,18 @@ const UsersController = require('../modules/users/usersController');
 
 const router = Router();
 
+router.get('/me', UsersController.me);
+router.post('/register', UsersController.register);
+router.post('/confirm-register', UsersController.confirmRegister);
+router.post('/login', UsersController.login);
+router.post('/forget-password', UsersController.forgetPassword);
+router.post('/reset-password', UsersController.resetPassword);
+router.put('/:id/password', UsersController.updatePassword);
+
 router.get('/', UsersController.findAll);
 router.get('/:id', UsersController.findOne);
 router.post('/', UsersController.create);
 router.put('/:id', UsersController.update);
 router.delete('/:id', UsersController.delete);
-
-router.post('/register', UsersController.register);
-router.post('/confirm-register', UsersController.confirmRegister);
-router.post('/login', UsersController.login);
 
 module.exports = router;
